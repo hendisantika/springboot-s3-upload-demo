c9ab488a4c367c7f8b8723234c54e746b6d0fe46
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 12:54:52 +0700

    #7 - Add convertMultiPartToFile in UploadController

--- src/main/java/com/hendisantika/springboots3uploaddemo/controller/UploadController.java
+++ src/main/java/com/hendisantika/springboots3uploaddemo/controller/UploadController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,6 +34,15 @@ public class UploadController {
 
     @Value("${bucketName}")
     private String bucketName;
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+
 
     private String uploadFileTos3bucket(String fileName, File file) {
         try {
