103360ae476b12423a03dbaa1f46b0f04fce06eb
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 13:16:39 +0700

    #12 - Add Enable Async & uploadFileToS3Bucket AmazonS3ClientService

--- src/main/java/com/hendisantika/springboots3uploaddemo/SpringbootS3UploadDemoApplication.java
+++ src/main/java/com/hendisantika/springboots3uploaddemo/SpringbootS3UploadDemoApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.springboots3uploaddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringbootS3UploadDemoApplication {
 
     public static void main(String[] args) {
