339c06a3c06ac14466a136a57b4a47dfcd4cc7d4
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 12:53:48 +0700

    #5 - Add UploadController

---
+++ src/main/java/com/hendisantika/springboots3uploaddemo/controller/UploadController.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springboots3uploaddemo.controller;
+
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-s3-upload-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/03/20
+ * Time: 12.53
+ */
+@RestController
+@RequestMapping("/home/")
+public class UploadController {
+
+    @Autowired
+    private AmazonS3 s3client;
+
+    @Value("${endpointUrl}")
+    private String endpointUrl;
+
+    @Value("${bucketName}")
+    private String bucketName;
+}
