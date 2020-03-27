7982fdbe80c3421b06146513761fc2d986e89999
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 13:22:18 +0700

    #15 - Add FileHandlerController

---
+++ src/main/java/com/hendisantika/springboots3uploaddemo/controller/FileHandlerController.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springboots3uploaddemo.controller;
+
+import com.hendisantika.springboots3uploaddemo.service.AmazonS3ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-s3-upload-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/03/20
+ * Time: 13.22
+ */
+@RestController
+@RequestMapping("/files")
+public class FileHandlerController {
+
+    @Autowired
+    private AmazonS3ClientService amazonS3ClientService;
+}
