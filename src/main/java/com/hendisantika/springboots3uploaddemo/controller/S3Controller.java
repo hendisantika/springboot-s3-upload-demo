c89ce319ed353d9929c42fa5f549474ac1bd15f1
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-29 06:02:39 +0700

    #24 - Add getFile in S3Controller

--- src/main/java/com/hendisantika/springboots3uploaddemo/controller/S3Controller.java
+++ src/main/java/com/hendisantika/springboots3uploaddemo/controller/S3Controller.java
@@ -2,7 +2,15 @@ package com.hendisantika.springboots3uploaddemo.controller;
 
 import com.hendisantika.springboots3uploaddemo.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,7 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 05.57
  */
 @RestController
+@RequestMapping("demo")
 public class S3Controller {
     @Autowired
     private FileService fileService;
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<ByteArrayResource> getFile(@PathVariable String id) throws IOException {
+        final ByteArrayResource byteArrayResource = new ByteArrayResource(this.fileService.getFile(id));
+        return ResponseEntity
+                .ok()
+                .contentLength(byteArrayResource.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header("Content-Disposition", "attachment; filename=" + id + ".csv")
+                .body(byteArrayResource);
+    }
 }
