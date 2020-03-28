fac8566a85684cacf5d7b61b69d52bc78c77322c
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-29 06:25:13 +0700

    #33 - Add uploadFile method S3BucketManager

--- src/main/java/com/hendisantika/springboots3uploaddemo/service/S3BucketManager.java
+++ src/main/java/com/hendisantika/springboots3uploaddemo/service/S3BucketManager.java
@@ -1,9 +1,15 @@
 package com.hendisantika.springboots3uploaddemo.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +28,34 @@ public class S3BucketManager {
 
     @Value("${aws.services.bucket}")
     private String bucketName;
+
+    /**
+     * Uploaded file to S3 bucket using amazon S3 client
+     *
+     * @param fileName
+     * @param file
+     * @param bucketName
+     */
+    public void uploadFile(final String fileName, final MultipartFile file,
+                           final String bucketName, final boolean allowPublicAccess) {
+
+        final ObjectMetadata data = new ObjectMetadata();
+        data.setContentType(file.getContentType());
+        data.setContentLength(file.getSize());
+
+        try {
+            final PutObjectRequest putObjectRequest;
+            if (allowPublicAccess) {
+                putObjectRequest = new PutObjectRequest(bucketName, fileName, file.getInputStream(), data)
+                        .withCannedAcl(CannedAccessControlList.PublicRead);
+            } else {
+                putObjectRequest = new PutObjectRequest(bucketName, fileName, file.getInputStream(), data);
+            }
+
+            amazonS3Client.putObject(putObjectRequest);
+        } catch (IOException e) {
+
+        }
+    }
 
 }
